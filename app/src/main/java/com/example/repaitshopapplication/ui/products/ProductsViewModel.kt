eadee1b66be973d1be76573ef4653fb491ad46d5
Tayeb-shahbakhsh <shahbakshtayyeb@gmail.com>
2022-06-12 04:51:57 -0600

    shabnam font added

--- app/src/main/java/com/example/repaitshopapplication/ui/products/ProductsViewModel.kt
+++ app/src/main/java/com/example/repaitshopapplication/ui/products/ProductsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.repaitshopapplication.ui.notifications
+package com.example.repaitshopapplication.ui.products
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ProductsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/repaitshopapplication/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/repaitshopapplication/ui/search/SearchFragment.kt
@@ -1,4 +1,4 @@
-package com.example.repaitshopapplication.ui.notifications
+package com.example.repaitshopapplication.ui.search
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.repaitshopapplication.databinding.FragmentNotificationsBinding
+import com.example.repaitshopapplication.databinding.FragmentSearchBinding
 
-class NotificationsFragment : Fragment() {
+class SearchFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentSearchBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(SearchViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
